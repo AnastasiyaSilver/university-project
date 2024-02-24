8ec3d709e64cfd7e9c8a6985160904ce18db3143
AnastasiyaSilver <silver.nas.ip@gmail.com>
2024-02-24 11:51:43 +0100

    Version 4 - final version.

--- src/main/java/pl/asilver/university/service/ProfessorService.java
+++ src/main/java/pl/asilver/university/service/ProfessorService.java
@@ -3,7 +3,8 @@ package pl.asilver.university.service;
 import pl.asilver.university.entity.Professor;
 
 public class ProfessorService {
-    public static void performProfessorAction(Professor professor){
+    public static void performProfessorAction(Professor professor) {
+        System.out.println("Professor:");
         professor.introduce();
         professor.teach();
     }
--- src/main/java/pl/asilver/university/service/StudentService.java
+++ src/main/java/pl/asilver/university/service/StudentService.java
@@ -5,6 +5,7 @@ import pl.asilver.university.entity.Student;
 
 public class StudentService {
     public static void performStudentAction(Student student) {
+        System.out.println("Student: ");
         student.introduce();
         student.study();
     }
